347509f7881240c2f07ea579067c40dbec78c602
Kleber Silva <kleber.swf@gmail.com>
2019-06-29 16:18:59 -0300

    using strict equality checks on tests

--- test/test.js
+++ test/test.js
@@ -6,28 +6,29 @@ describe('basic merge', function () {
 		const tgt = { a: 2 };
 		const src = { a: 1 };
 		const res = merge(tgt, src);
-		assert.equal(res.a, src.a)
+		assert.strictEqual(res.a, src.a)
 	});
 
 	it('should create new values', function () {
 		const tgt = {};
 		const src = { a: 2 };
 		const res = merge(tgt, src);
-		assert.equal(res.a, src.a)
+		assert.strictEqual(res.a, src.a)
 	});
 
 	it('should merge all basic types', function () {
 		const tgt = { boolValue: false, numberValue: 100, stringValue: 'target' };
 		const src = { boolValue: true, numberValue: 222, stringValue: 'source' };
 		const res = merge(tgt, src);
-		assert.deepEqual(res, src);
+		assert.deepStrictEqual(res, src);
 	});
 
 	it('should return a clone', function () {
 		const tgt = { boolValue: false, numberValue: 100, stringValue: 'target' };
 		const src = { boolValue: true, numberValue: 222, stringValue: 'source' };
 		const res = merge(tgt, src);
-		assert.notEqual(res, src);
+		assert.notStrictEqual(res, tgt);
+		assert.notStrictEqual(res, src);
 	});
 
 	// TODO merge with null values
@@ -41,7 +42,7 @@ describe('object merge', function () {
 		const tgt = { a: '@', b: { c: 1, d: { e: 2, f: { g: 3, h: { i: 4, l: '$' } } } } };
 		const src = { b: { c: 10, d: { e: 20, f: { g: 30, h: { i: 40, j: { k: 50 } } } } } };
 		const res = merge(tgt, src);
-		assert.deepEqual(res, {
+		assert.deepStrictEqual(res, {
 			a: '@', b: { c: 10, d: { e: 20, f: { g: 30, h: { i: 40, j: { k: 50 }, l: '$' } } } }
 		});
 	});
@@ -50,7 +51,7 @@ describe('object merge', function () {
 		const tgt = { a: { b: { c: 1 }, c: 1, d: 1 } };
 		const src = { a: { b: 10, c: { d: 20 } } };
 		const res = merge(tgt, src);
-		assert.deepEqual(res, { a: { b: 10, c: { d: 20 }, d: 1 } });
+		assert.deepStrictEqual(res, { a: { b: 10, c: { d: 20 }, d: 1 } });
 	});
 });
 
