ffed19a7e1d1481648229005b258bd46b17d4d01
Kleber Silva <kleber.swf@gmail.com>
2019-06-29 12:38:32 -0300

    checkpoint

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const merge = function (target, source) {
+	const result = Object.assign({}, target);
+	return mergeObject(result, source);
+}
+
 const mergeObject = function (target, source) {
 	Object.keys(source).forEach(key => {
 		target[key] = _merge(key, target, source)
@@ -23,8 +28,9 @@ const _merge = function (key, target, source) {
 		const targetValue = target[key];
 		if (Array.isArray(targetValue))
 			return mergeArray(targetValue, source[key]);
-		if (typeof targetValue === 'object')
+		if (typeof targetValue === 'object') {
 			return mergeObject(targetValue, source[key]);
+		}
 	}
 	return source[key];
 }
@@ -33,9 +39,47 @@ const _mergeArrayWithParams = function (target, source) {
 	return source;
 }
 
-const merge = function (target, source) {
-	const result = Object.assign({}, target);
-	return mergeObject(result, source);
-}
-
 module.exports = merge;
+
+//////////////////////////////////////////
+
+const tgt = {
+	a: {
+		b: { c: 2 }
+	}
+};
+const src = {
+	a: {
+		b: 10,
+		c: { d: 20 }
+	}
+};
+const res = merge(tgt, src);
+console.log(res);
+
+// const a = {
+// 	// test1: 'a',
+// 	// test2: 1,
+// 	// test4: 'nope',
+// 	test1: [0, 1, 2],
+// 	test2: [0],
+// 	test3: [],
+// 	test4: [0, 1, 2, 3, 4],
+// 	test5: [0, 1, 2]
+// }
+
+// const b = {
+// 	// test1: 'b',
+// 	// test2: 2,
+// 	// test3: true,
+// 	test1: [10, 11, 12, 13],
+// 	test2: [10, 11],
+// 	test3: [10, 11, 12],
+// 	test4: [10, 11],
+// 	test5: []
+// }
+
+// const result = merge(a, b);
+// console.log('target', a);
+// console.log('source', b);
+// console.log('result', result);
--- test/test.js
+++ test/test.js
@@ -1,29 +1,63 @@
 const assert = require('assert');
 const merge = require('../index');
 
-describe('merge', function () {
+describe('basic merge', function () {
 	it('should prefer source values', function () {
 		const tgt = { a: 2 };
 		const src = { a: 1 };
 		const res = merge(tgt, src);
 		assert.equal(res.a, src.a)
 	});
+
 	it('should create new values', function () {
 		const tgt = {};
 		const src = { a: 2 };
 		const res = merge(tgt, src);
 		assert.equal(res.a, src.a)
 	});
+
 	it('should merge all basic types', function () {
 		const tgt = { boolValue: false, numberValue: 100, stringValue: 'target' };
 		const src = { boolValue: true, numberValue: 222, stringValue: 'source' };
 		const res = merge(tgt, src);
 		assert.deepEqual(res, src);
 	});
+
 	it('should return a clone', function () {
 		const tgt = { boolValue: false, numberValue: 100, stringValue: 'target' };
 		const src = { boolValue: true, numberValue: 222, stringValue: 'source' };
 		const res = merge(tgt, src);
 		assert.notEqual(res, src);
+	});
+
+	// TODO merge arrays
+	// TODO merge with null values
+	// TODO null target
+	// TODO null source
+});
+
+
+describe('object merge', function () {
+	it('should deeply objects', function () {
+		const tgt = { a: { b: 1, c: { d: 2, e: { f: 3, g: { h: 4 } } } } };
+		const src = { a: { b: 10, c: { d: 20, e: { f: 30, g: { h: 40, i: { j: 50 } } } } } };
+		const res = merge(tgt, src);
+		assert.deepEqual(res, src);
+	});
+
+	it('should deeply objects', function () {
+		const tgt = {
+			a: {
+				b: { c: 2 }
+			}
+		};
+		const src = {
+			a: {
+				b: 10,
+				c: { d: 20 }
+			}
+		};
+		const res = merge(tgt, src);
+		assert.deepEqual(res, { a: { b: 10, c: { d: 10 } } });
 	});
 });
