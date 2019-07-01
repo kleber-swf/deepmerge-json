45c7f62f048777a51944b0bc032402524855a9b8
Kleber Silva <kleber.swf@gmail.com>
2019-07-01 11:38:16 -0300

    using typescript

---
+++ README.md
@@ -0,0 +1,2 @@
+# Deepmerge Json
+
--- index.ts
+++ index.ts
@@ -1,31 +1,31 @@
-const directReplace = (_, pos) => pos;
-const cloneReplace = (_, pos) => Object.assign({}, pos);
+const directReplace = (_: any, pos: any) => pos;
+const cloneReplace = (_: any, pos: any) => Object.assign({}, pos);
 
-const mergeObjects = function (pre, pos) {
+const mergeObjects = function (pre: any, pos: any) {
 	pre = Object.assign({}, pre);
 	Object.keys(pos).forEach(k =>
 		pre[k] = merge(pre[k], pos[k]));
 	return pre;
 }
 
-const mergeArrays = function (pre, pos) {
+const mergeArrays = function (pre: any[], pos: any[]) {
 	pre = pre.slice();
 	pos.forEach((v, i) => pre[i] = v);
 	return pre;
 }
 
-const mergeArrayWithParams = function (pre, pos) {
+const mergeArrayWithParams = function (pre: any[], pos: any) {
 	pre = pre.slice();
-	const key = Object.keys(pos)[0]; // TODO
+	const key = Object.keys(pos)[0]; // (x_x) This is ugly
 	if (key in arrayMergeFn)
 		return arrayMergeFn[key](pre, pos[key]);
 	return pos;
 }
 
 const arrayMergeFn = {
-	$append: (pre, pos) => pre.concat(pos),
-	$prepend: (pre, pos) => pos.concat(pre),
-	$set: (_, pos) => pos.slice()
+	$append: (pre: any[], pos: any[]) => pre.concat(pos),
+	$prepend: (pre: any[], pos: any[]) => pos.concat(pre),
+	$set: (_: any, pos: any[]) => pos.slice()
 }
 
 const fn = {
@@ -42,10 +42,8 @@ const fn = {
 	ba: cloneReplace
 }
 
-const merge = function (target, source) {
+export default function merge(target: any, source: any) {
 	const tt = Array.isArray(target) ? 'a' : (typeof target === 'object' ? 'o' : 'b');
 	const st = Array.isArray(source) ? 'a' : (typeof source === 'object' ? 'o' : 'b');
 	return fn[tt + st](target, source);
 }
-
-module.exports = merge;
