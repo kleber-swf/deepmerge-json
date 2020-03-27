3c59bbfa8439ee31a740ad797c381e93a6011472
Kleber Lopes da Silva <kleber.swf@gmail.com>
2020-03-27 13:18:16 -0300

    added $replace operation: code and test

--- index.js
+++ index.js
@@ -2,33 +2,40 @@ const directReplace = (_, pos) => pos;
 const cloneReplace = (_, pos) => Object.assign({}, pos);
 const cloneArray = (_, pos) => pos.slice();
 
-const mergeObjects = function (pre, pos) {
+const mergeObjects = function(pre, pos) {
 	pre = Object.assign({}, pre);
-	Object.keys(pos).forEach(k =>
-		pre[k] = merge(pre[k], pos[k]));
+	Object.keys(pos).forEach(k => (pre[k] = merge(pre[k], pos[k])));
 	return pre;
-}
+};
 
-const mergeArrays = function (pre, pos) {
+const mergeArrays = function(pre, pos) {
 	pre = pre.slice();
-	pos.forEach((v, i) => pre[i] = merge(pre[i], v));
+	pos.forEach((v, i) => (pre[i] = merge(pre[i], v)));
 	return pre;
-}
+};
 
-const mergeArrayWithParams = function (pre, pos) {
+const mergeArrayWithParams = function(pre, pos) {
 	pre = pre.slice();
 	const key = Object.keys(pos)[0]; // (x_x) This is ugly
-	if (key in arrayMergeFn)
-		return arrayMergeFn[key](pre, pos[key]);
+	if (key in arrayMergeFn) return arrayMergeFn[key](pre, pos[key]);
 	return pos;
-}
+};
+
+const indexedReplace = function(pre, pos) {
+	pre = pre.slice();
+	Object.keys(pos).forEach(k => {
+		pre[k] = merge(pre[k], pos[k]);
+	});
+	return pre;
+};
 
 const arrayMergeFn = {
 	$push: (pre, pos) => pre.concat(pos),
 	$append: (pre, pos) => pre.concat(pos),
 	$prepend: (pre, pos) => pos.concat(pre),
+	$replace: indexedReplace,
 	$set: cloneArray
-}
+};
 
 const fn = {
 	oo: mergeObjects,
@@ -42,14 +49,13 @@ const fn = {
 	bb: directReplace,
 	bo: cloneReplace,
 	ba: cloneArray
-}
+};
 
 function merge(pre, pos) {
 	if (!pos) return pos;
-	const tt = Array.isArray(pre) ? 'a' : (typeof pre === 'object' ? 'o' : 'b');
-	const st = Array.isArray(pos) ? 'a' : (typeof pos === 'object' ? 'o' : 'b');
+	const tt = Array.isArray(pre) ? 'a' : typeof pre === 'object' ? 'o' : 'b';
+	const st = Array.isArray(pos) ? 'a' : typeof pos === 'object' ? 'o' : 'b';
 	return fn[tt + st](pre, pos);
 }
 
 export default merge;
-
